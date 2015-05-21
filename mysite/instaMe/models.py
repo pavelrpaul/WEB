b92f20f5bdd0fdd466e396f70f2b0d3b1d29c2db
pavelrpaul <pavelrpaul@users.noreply.github.com>
2015-05-21 12:02:25 -0700

    add templates

--- mysite/instaMe/models.py
+++ mysite/instaMe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    # filename = models.CharField(max_length=50, default='default.png')
+    filename = models.ImageField(upload_to='/uploads/', default='default.png')
+
+    def __unicode__(self):
+        return str(self.user.username)
